5237c031c94e3f33918e28060728d41050b5d007
Oyuka <58796152+Oyuka100@users.noreply.github.com>
2020-05-06 22:23:23 -0400

    some changes

--- 02-Homework/Develop/routes/apiRoutes.js
+++ 02-Homework/Develop/routes/apiRoutes.js
@@ -1,56 +1,50 @@
 var notes = require("../db/db");
-var  fs = require("fs");
+var fs = require("fs");
 const router = require("express").Router();
 
+router.get("/notes", function (req, res) {
+  res.json(notes);
+  //   console.log("notes retrieved");
+  //   if (!notes === undefined || !notes.length === 0) {
+  //     for (let i = 0; i < notes.length; i++) {
+  //       notes[i].id = i;
+  //     }
+  //   }
+});
 
+router.post("/notes", function (req, res) {
+  let newNote = req.body;
 
-    router.get("/api/notes", function(req, res) {
-        res.json(notes);
-        console.log("notes retrieved")
-        if(!notes === undefined || !notes.length === 0){
-            
-        for(let i = 0 ; i < notes.length ; i ++){
-            notes[i].id = i 
-        }
-    }
-      });
+  if (notes === "") {
+    newNote.id = 0;
 
+    let noteString = JSON.stringify(newNote);
 
-      router.post("/api/notes", function(req, res) {
-        
-       if (notes===""){
-           req.body.id = 0;
+    fs.writeFile("db/db.json", noteString, function (err) {
+      if (err) throw err;
+      console.log("note sent");
+    });
+  } else {
+    newNote.id = notes.length;
+    console.log(notes);
+    notes.push(newNote);
 
+    let noteString = JSON.stringify(notes);
 
-        let newNote = JSON.stringify(req.body);
-        
-        fs.writeFile("db/db.json", newNote, function(err){
-            if(err) throw err;
-            console.log("note sent")
-        });
-        }else{
-            res.body.id = notes.length
-            notes.push(req.body);
-            console.log(notes);
+    fs.writeFile("db/db.json", noteString, function (err) {
+      if (err) throw err;
+      console.log("note sent");
+    });
+  }
+});
 
-            let newNote = JSON.stringify(req.body);
-        
-        fs.writeFile("db/db.json", newNote, function(err){
-            if(err) throw err;
-            console.log("note sent")
-        });
-        }
-
-      });
-
-      router.delete("/api/notes/:id", function(req, res){
-        let nodeId = req.params.id 
-        notes.splice(noteId, 1)
-        let updatedNts = JSON.stringify(notes);
-        fs.writeFile("db/db.json", updatedNts, function(err){
-            if(err) throw err;
-            console.log("note deleted")
-        });
-
-      });
- module.exports = router;
+router.delete("/notes/:id", function (req, res) {
+  let nodeId = req.params.id;
+  notes.splice(nodeId, 1);
+  let updatedNts = JSON.stringify(notes);
+  fs.writeFile("db/db.json", updatedNts, function (err) {
+    if (err) throw err;
+    console.log("note deleted");
+  });
+});
+module.exports = router;
